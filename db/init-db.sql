3354af8ac697f3a23b078838b39c8dc1326da7e6
patawa91 <patawa91@gmail.com>
2024-05-10 21:09:21 -0500

    Folder structure, initial files, and docker.

--- db/init-db.sql
+++ db/init-db.sql
@@ -15,12 +15,12 @@ CHECK (type IN ('checking', 'savings', 'credit'));
 INSERT INTO accounts 
     (account_number, name, amount, type, credit_limit)
 VALUES
-    (1, 'Johns Checking', 1000, 'checking'),
-    (2, 'Janes Savings', 2000, 'savings'),
+    (1, 'Johns Checking', 1000, 'checking', NULL),
+    (2, 'Janes Savings', 2000, 'savings', NULL),
     (3, 'Jills Credit', -3000, 'credit', 10000),
-    (4, 'Bobs Checking', 40000, 'checking'),
-    (5, 'Bills Savings', 50000, 'savings'),
+    (4, 'Bobs Checking', 40000, 'checking', NULL),
+    (5, 'Bills Savings', 50000, 'savings', NULL),
     (6, 'Bills Credit', -60000, 'credit', 60000),
-    (7, 'Nancy Checking', 70000, 'checking'),
-    (8, 'Nancy Savings', 80000, 'savings'),
+    (7, 'Nancy Checking', 70000, 'checking', NULL),
+    (8, 'Nancy Savings', 80000, 'savings', NULL),
     (9, 'Nancy Credit', -90000, 'credit', 100000);
