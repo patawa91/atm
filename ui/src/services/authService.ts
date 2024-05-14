867df4c44d7c24e61ee546c854a25e3e3222dc98
patawa91 <patawa91@gmail.com>
2024-05-14 17:07:32 -0500

    clear ui

--- ui/src/services/authService.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export async function login(accountNumber: string) {
-
-  const response = await axios.post('/auth/login', { accountNumber });
-  return response.data.token;
-}
